974acd706839d5d28de30dac5367c58a08478afb
Jason Stanley <jasonstanleyyoman@gmail.com>
2024-12-26 19:46:55 +0700

    add connection handler

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 #![allow(unused_imports)]
-use std::net::TcpListener;
+use std::{
+    io::{BufReader, Write},
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -12,6 +15,7 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(_stream) => {
+                handle_connection(&_stream);
                 println!("accepted new connection");
             }
             Err(e) => {
@@ -19,4 +23,16 @@ fn main() {
             }
         }
     }
+}
+
+fn handle_connection(mut stream: &TcpStream) {
+    let buf_reader = BufReader::new(stream);
+    let message_size: [u8; 4] = [0; 4];
+    let correlation_id: [u8; 4] = [0, 0, 0, 7];
+    stream
+        .write_all(&message_size)
+        .expect("fail to write message size");
+    stream
+        .write_all(&correlation_id)
+        .expect("fail to write correlation id");
 }
