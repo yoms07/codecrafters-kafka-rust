cb98a65b0b7022ae8c4ba216865277acc414a580
Jason Stanley <jasonstanleyyoman@gmail.com>
2024-12-26 19:35:14 +0700

    add tcp listener

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,16 @@ fn main() {
 
     // Uncomment this block to pass the first stage
     //
-    // let listener = TcpListener::bind("127.0.0.1:9092").unwrap();
-    //
-    // for stream in listener.incoming() {
-    //     match stream {
-    //         Ok(_stream) => {
-    //             println!("accepted new connection");
-    //         }
-    //         Err(e) => {
-    //             println!("error: {}", e);
-    //         }
-    //     }
-    // }
+    let listener = TcpListener::bind("127.0.0.1:9092").unwrap();
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(_stream) => {
+                println!("accepted new connection");
+            }
+            Err(e) => {
+                println!("error: {}", e);
+            }
+        }
+    }
 }
