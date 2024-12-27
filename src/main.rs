7e84a71a2dbbdf7b1cc41ef947366ff249c3c4c7
Jason Stanley <jasonstanleyyoman@gmail.com>
2024-12-27 11:28:28 +0700

    move request to separate module

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,10 @@ use std::{
 
 use bytes::{buf, Buf, BufMut};
 
+mod protocol;
+
+use protocol::request::Request;
+
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
@@ -29,51 +33,10 @@ fn main() {
     }
 }
 
-#[derive(Debug)]
-struct Request {
-    message_size: u32,
-    request_api_key: u16,
-    request_api_version: u16,
-    correlation_id: u32,
-    data: Vec<u8>,
-}
-
-impl Request {
-    fn new<T: Read>(mut stream: T) -> Result<Request, Box<dyn Error>> {
-        let mut buffer: [u8; 1024] = [0; 1024];
-        stream.read(&mut buffer)?;
-
-        let mut request = buffer.as_slice();
-
-        let message_size: u32 = request.get_u32();
-        let request_api_key = request.get_u16();
-        let request_api_version = request.get_u16();
-        let correlation_id = request.get_u32();
-
-        Ok(Request {
-            message_size,
-            request_api_key,
-            request_api_version,
-            correlation_id,
-            data: vec![0],
-        })
-    }
-    fn log(&self) {
-        println!("[REQUEST] message_size: {}", self.message_size);
-        println!("[REQUEST] request_api_key: {}", self.request_api_key);
-        println!(
-            "[REQUEST] request_api_version: {}",
-            self.request_api_version
-        );
-        println!("[REQUEST] correlation_id: {}", self.correlation_id);
-        println!("[REQUEST] data: {:?}", self.data);
-    }
-}
-
 fn handle_connection(mut stream: &TcpStream) {
     let mut buf_reader = BufReader::new(stream);
     let mut buf_writer = BufWriter::new(&mut stream);
-    let request = Request::new(&mut buf_reader).unwrap();
+    let request = Request::new(&mut buf_reader).expect("Error parsing request");
     request.log();
 
     buf_writer
---
+++ src/protocol/request.rs
@@ -0,0 +1,44 @@
+use std::{error::Error, io::Read};
+
+use bytes::Buf;
+
+#[derive(Debug)]
+pub struct Request {
+    pub message_size: u32,
+    pub request_api_key: u16,
+    pub request_api_version: u16,
+    pub correlation_id: u32,
+    pub data: Vec<u8>,
+}
+
+impl Request {
+    pub fn new<T: Read>(mut stream: T) -> Result<Request, Box<dyn Error>> {
+        let mut buffer: [u8; 1024] = [0; 1024];
+        stream.read(&mut buffer)?;
+
+        let mut request = buffer.as_slice();
+
+        let message_size: u32 = request.get_u32();
+        let request_api_key = request.get_u16();
+        let request_api_version = request.get_u16();
+        let correlation_id = request.get_u32();
+
+        Ok(Request {
+            message_size,
+            request_api_key,
+            request_api_version,
+            correlation_id,
+            data: vec![0],
+        })
+    }
+    pub fn log(&self) {
+        println!("[REQUEST] message_size: {}", self.message_size);
+        println!("[REQUEST] request_api_key: {}", self.request_api_key);
+        println!(
+            "[REQUEST] request_api_version: {}",
+            self.request_api_version
+        );
+        println!("[REQUEST] correlation_id: {}", self.correlation_id);
+        println!("[REQUEST] data: {:?}", self.data);
+    }
+}
