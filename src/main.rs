64ef780c5649577d95cc37fef0d9d9ec9b1c2aed
Jason Stanley <jasonstanleyyoman@gmail.com>
2024-12-27 12:59:38 +0700

    looping serial request

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,17 @@ use std::{
     error::Error,
     io::{BufRead, BufReader, BufWriter, Read, Write},
     net::{TcpListener, TcpStream},
+    time::Duration,
 };
 
 use bytes::{buf, Buf, BufMut};
 
 mod protocol;
 
-use protocol::{request::Request, response::Response};
+use protocol::{
+    request::{Request, RequestError},
+    response::Response,
+};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -34,35 +38,45 @@ fn main() {
 }
 
 fn handle_connection(mut stream: &TcpStream) {
-    let mut buf_reader = BufReader::new(stream);
-    let mut buf_writer = BufWriter::new(&mut stream);
-    let request = Request::new(&mut buf_reader).expect("Error parsing request");
-    request.log();
+    stream
+        .set_read_timeout(Some(Duration::new(5, 0)))
+        .expect("cannot set read timeout");
+    loop {
+        let mut buf_reader = BufReader::new(stream);
+        let mut buf_writer = BufWriter::new(&mut stream);
+        let request = match Request::new(&mut buf_reader) {
+            Ok(r) => r,
+            Err(RequestError::ClientDisconnected) => {
+                break;
+            }
+        };
+        request.log();
 
-    let mut response = Response::build_from_request(&request);
+        let mut response = Response::build_from_request(&request);
 
-    match request.request_api_key {
-        18 => {
-            if request.request_api_version <= 4 {
-                response.body.put_u16(0); // error code
-                response.body.put_u8(2); // array length + 1
-                                         // first element
-                response.body.put_u16(18); // api key
-                response.body.put_u16(0); // min version
-                response.body.put_u16(4); // max version
-                response.body.put_u8(0); // TAG_BUFFER length
-                response.body.put_u32(0); // Throttle time
-                response.body.put_u8(0); // TAG_BUFFER length
-            } else {
-                response.body.put_u16(35);
+        match request.request_api_key {
+            18 => {
+                if request.request_api_version <= 4 {
+                    response.body.put_u16(0); // error code
+                    response.body.put_u8(2); // array length + 1
+                                             // first element
+                    response.body.put_u16(18); // api key
+                    response.body.put_u16(0); // min version
+                    response.body.put_u16(4); // max version
+                    response.body.put_u8(0); // TAG_BUFFER length
+                    response.body.put_u32(0); // Throttle time
+                    response.body.put_u8(0); // TAG_BUFFER length
+                } else {
+                    response.body.put_u16(35);
+                }
+            }
+            _ => {
+                response.body.put_u16(35); // error code
             }
         }
-        _ => {
-            response.body.put_u16(35); // error code
-        }
-    }
 
-    response
-        .send(&mut buf_writer)
-        .expect("error sending response");
+        response
+            .send(&mut buf_writer)
+            .expect("error sending response");
+    }
 }
