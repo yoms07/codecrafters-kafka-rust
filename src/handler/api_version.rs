ef059f639d1efa18cd769dbb01edba54ed49960a
Jason Stanley <dummy.jason00@gmail.com>
2024-12-28 20:49:14 +0700

    add support for fetch request

--- src/handler/api_version.rs
+++ src/handler/api_version.rs
@@ -2,21 +2,41 @@ use bytes::BufMut;
 
 use crate::protocol::{request::Request, response::Response};
 
+struct SupportedAPI {
+    pub api_key: u16,
+    pub min_version: u16,
+    pub max_version: u16,
+}
+
+static SUPPORTED_APIS: &[SupportedAPI] = &[
+    SupportedAPI {
+        api_key: 18,
+        min_version: 0,
+        max_version: 4,
+    },
+    SupportedAPI {
+        api_key: 75,
+        min_version: 0,
+        max_version: 4,
+    },
+    SupportedAPI {
+        api_key: 1,
+        min_version: 0,
+        max_version: 16,
+    },
+];
+
 pub fn handle(req: &Request, res: &mut Response) {
     if req.request_api_version <= 4 {
         res.body.put_u16(0); // error code
-        res.body.put_u8(3); // array length + 1
+        res.body.put_u8((SUPPORTED_APIS.len() + 1) as u8); // array length + 1
 
-        res.body.put_u16(18); // api key
-        res.body.put_u16(0); // min version
-        res.body.put_u16(4); // max version
-        res.body.put_u8(0); // TAG_BUFFER length
-
-        res.body.put_u16(75); // api key
-        res.body.put_u16(0); // min version
-        res.body.put_u16(4); // max version
-        res.body.put_u8(0); // TAG_BUFFER length
-
+        for api in SUPPORTED_APIS {
+            res.body.put_u16(api.api_key); // api key
+            res.body.put_u16(api.min_version); // min version
+            res.body.put_u16(api.max_version); // max version
+            res.body.put_u8(0); // TAG_BUFFER length
+        }
         res.body.put_u32(0); // Throttle time
         res.body.put_u8(0); // TAG_BUFFER length
     } else {
